5d609ed01ebf690d9bac6e0b2058af500e6b6c52
rishigardenshotel-alt <rishigardenshotel@gmail.com>
2026-05-12 20:09:28 +0530

    Delete hotelrishigardens directory

--- hotelrishigardens/schemaTypes/index.js
+++
@@ -1,6 +0,0 @@
-import room from './room'
-import post from './post'
-import bookingSite from './bookingSite'
-import siteSettings from './siteSettings'
-
-export const schemaTypes = [room, post, bookingSite, siteSettings]
